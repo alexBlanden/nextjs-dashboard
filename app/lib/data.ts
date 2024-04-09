b1ce48e89762616cc01190340792380db14a5e22
alexBlanden <me@alexblanden.co.uk>
2024-04-09 15:35:17 +0100

    Streaming and Suspense

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -48,6 +48,7 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+    console.log(latestInvoices)
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
